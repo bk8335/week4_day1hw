71d6d13a4424cc673f4a77d4b4b9e62798bd8e88
Benjamin <benjamin.kinnard1@gmail.com>
2017-03-27 21:48:25 +0100

    manually added if player 1 or 2 wins

--- controller.rb
+++ controller.rb
@@ -2,8 +2,16 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/rock_paper_scissors')
 
-get '/play/:choice1/:choice2' do
+get '/:choice1/:choice2' do
   rps = RockPaperScissors.new(params[:choice1], params[:choice2])
   @winner = rps.play
   erb(:results)
+end
+
+get '/rules' do
+  erb(:rules)
+end
+
+get '/home' do
+  erb(:home)
 end
